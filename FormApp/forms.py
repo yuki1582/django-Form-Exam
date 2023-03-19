fc52eed82f1e4df7be0557b2b0a49cdd7c80ec32
yuki1582 <yuki.nakamura1103@gmail.com>
2023-03-19 22:25:33 +0900

    form_update_delete_add

---
+++ FormApp/forms.py
@@ -0,0 +1,22 @@
+from .models import Students
+from django import forms
+
+class StudentInsertForm(forms.ModelForm):
+
+    name = forms.CharField(label='名前')
+    age = forms.IntegerField(label='年齢')
+    grade = forms.IntegerField(label='学歴')
+    picture = forms.FileField(label='ファイルアップロード')
+
+    class Meta:
+        model = Students
+        fields = '__all__'
+
+class StudentUpdateForm(forms.Form):
+    name = forms.CharField(label='名前')
+    age = forms.IntegerField(label='年齢')
+    grade = forms.IntegerField(label='学歴')
+    picture = forms.FileField(label='ファイルアップロード', required=False)
+
+class StudentDeleteForm(forms.Form):
+    id = forms.IntegerField(widget=forms.HiddenInput)
